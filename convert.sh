24cc995487cf917f222dabb97a0bad9eb711bc81
Iridium-Lo <80277972+Iridium-Lo@users.noreply.github.com>
2021-06-16 08:45:56 +0100

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -5,11 +5,13 @@ epubDir=~/Desktop/epubs
 cats=`ls $pdfDir`
 
 for i in `echo "$cats"`; do  
-   [ ! -d $epubDir/$i ]  && mkdir $epubDir/$i   
+   [ ! -d $epubDir/$i ]  && 
+      mkdir $epubDir/$i   
 done
 
 for i in `echo "$cats"`; do pushd $pdfDir/$i
-   for x in `ls *.pdf`; do book=`echo "$x" | sed -e s/.pdf//g`
+   for x in `ls *.pdf`; do 
+      book=`echo "$x" | sed -e s/.pdf//g`
       [[ ! -f $epubDir/$i/$book.epub ]] &&
          ebook-convert $book.pdf $epubDir/$i/$book.epub
          tput setaf 2 
