fc481c2086c66a3f7823b7c57a22926e30145b5d
iridium <iridium@yourmum.com>
2021-06-16 14:31:59 +0100

    x

--- bookConvert/convert.sh
+++ bookConvert/convert.sh
@@ -4,22 +4,24 @@ epubDir=~/Desktop/epubs
 
 cats=`ls $pdfDir`
 
-for i in `echo "$cats"`; do  
-   [ ! -d $epubDir/$i ]  && 
-      mkdir -p $epubDir/$i   
+for i in `echo "$cats"`; do
+   [ ! -d $epubDir/$i ] \
+   && mkdir -p $epubDir/$i
 done
 
-for i in `echo "$cats"`; do pushd $pdfDir/$i
-   for x in `ls *.pdf`; do 
-      book=`echo "$x" | sed -e s/.pdf//g`
-      [[ ! -f $epubDir/$i/$book.epub ]] &&
-         ebook-convert $book.pdf   \
-            $epubDir/$i/$book.epub \
-            > /dev/null
-         tput setaf 2 
-         echo "$book.epub -> done"
-         tput sgr0
-   done
-   popd
-done   
-popd
+for i in `echo "$cats"`; do 
+   cd $pdfDir/$i
+   tput setaf 7
+   echo -e "\n[$i]"
+      for x in `ls *.pdf`; do 
+         book=`echo "$x" | sed -e s/.pdf//g`
+         [[ ! -f $epubDir/$i/$book.epub ]] \
+         && ebook-convert $book.pdf        \
+               epubDir/$i/$book.epub       \
+               > /dev/null 2>&1
+            tput setaf 2
+            echo "-> $book.epub"
+            tput sgr0
+            cd - > /dev/null
+      done
+done
